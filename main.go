7d6a68f8e8da3df4cd7aa21c8fba0b77d54c33b8
Alexander Trakhimenok <alex@trakhimenok.com>
2026-01-03 16:34:14 +0000

    chore(CI): release workflow

--- main.go
+++ main.go
@@ -10,7 +10,19 @@ import (
 	"github.com/ingitdb/ingitdb-go/ingitdb"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+
+	if os.Args[1] == "--version" {
+		fmt.Printf("ingitdb %s (%s) @ %s\n", version, commit, date)
+		return
+	}
+
 	dirPath := expandHome(os.Args[1])
 	log.Println("inGitDB db path: ", dirPath)
 
